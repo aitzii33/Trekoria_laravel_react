a7c7c4fd4156dc15c9be99490726fd52bd0eaac5
Aitziber <aitzibergonzalez14@gmail.com>
2026-02-03 14:31:59 +0100

    dataseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -31,7 +31,14 @@ class DatabaseSeeder extends Seeder
             'country' => 'Spain', 
             'continent' => 'Europe'
         ]);
-        
+
+        Activities::create([
+            'place_id' => 1,
+            'name' => 'ride a horse',
+            'description' => 'It would be a group of 5 persons and a instructore. This activity has a 40 minutes of duration.',
+            'location' => 'C. de Medellín, nº10, 4º-1',
+        ]);
+
 
         User::create(
         ['email' => 'shannon@example.com'],
@@ -41,10 +48,7 @@ class DatabaseSeeder extends Seeder
             'user_name' => 'testuser',
             'birth_day' => '1998-05-12',
             'type_user' => 1,
-            'image' => '../img/Girl.avif',
             'password' => Hash::make('12345'),
         ]);
-        
-        User::factory(40)->create();
     }
 }
